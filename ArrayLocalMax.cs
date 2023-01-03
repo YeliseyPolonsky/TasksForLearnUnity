63743c107d75ada56c96de5e91cd834ccfc05349
YeliseyPolonsky <elickpro228@gmail.com>
2023-01-03 22:10:48 +0300

    Tests
    test

---
+++ ArrayLocalMax.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp31
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WindowWidth += 10;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+
+            int Lenght = 30;
+            int[] Array = new int[30];
+            List<int> LocalsMax=new List<int>();
+            Random rand = new Random();
+
+            Console.Write("Массив : ");
+            for (int i = 1; i < Array.Length - 1; i++)
+            {
+                Array[i] = rand.Next(0, 101);
+                Console.Write(Array[i] + " ");
+            }
+
+
+            for (int i = 1; i < Array.Length-1; i++)
+            {
+                if (Array[i] > Array[i - 1] && Array[i] > Array[i + 1])
+                    LocalsMax.Add(Array[i]);
+            }
+            if(Array[0] > Array[1])
+                LocalsMax.Add(Array[0]);
+            if (Array[Lenght-1] > Array[Lenght - 2])
+                LocalsMax.Add(Array[Lenght - 1]);
+
+
+            Console.Write("\n\nЛокальные максимумы: ");
+            foreach (var item in LocalsMax)
+            {
+                Console.Write(item+ " ");
+            }
+
+            Console.ReadKey(true);
+
+
+
+
+        }
+    }
+}
---
+++ Triangles.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp31
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.Clear();
+
+            Console.WriteLine("Введите:\n1- треугольник обычный.\n2 - треуголник перевернутый.");
+            int EulerAngel = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Какой высоты треугольник? : ");
+            int CountLine = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            if (EulerAngel == 1)
+            {
+                int K = 1;
+                for (int i = 0; i < CountLine; i++)
+                {
+
+                    for (int j = 0; j < K; j++)
+                    {
+                        Console.Write("*");
+                    }
+                    ++K;
+                    Console.WriteLine();
+                }
+
+            }
+
+            else
+            {
+                int K = CountLine;
+                for (int i = 0; i < CountLine; i++)
+                {
+                    for (int j = 0; j < K; j++)
+                    {
+                        Console.Write("*");
+                    }
+                    K--;
+                    Console.WriteLine();
+                }
+            }
+            Console.ReadKey(true);
+
+
+
+
+        }
+    }
+}
