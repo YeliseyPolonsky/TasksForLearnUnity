ba8d26e3fbf27ef5ab079b25f2dd47c4fb9164db
YeliseyPolonsky <elickpro228@gmail.com>
2023-01-14 21:44:15 +0300

    Create OOП_firstTask.cs

---
+++ OOП_firstTask.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp37
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player knight = new Player("Рыцарь", 100, 50, 45);
+            knight.ShowInfo();
+        }
+    }
+
+    class Player
+    {
+        private string _name;
+        private int _health;
+        private int _armor;
+        private int _damage;
+
+        public Player(string name, int health, int armor, int damage)
+        {
+            _name = name;
+            _health = health;
+            _armor = armor;
+            _damage = damage;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Имя - {_name}.\nЗдоровье - {_health}.\nБроня - {_armor}.\nУрон - {_damage}");
+        }
+    }
+}
