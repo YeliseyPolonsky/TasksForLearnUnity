293ba574ea10e1d991e527063d9421e809845134
YeliseyPolonsky <121894577+YeliseyPolonsky@users.noreply.github.com>
2023-01-15 15:11:49 +0300

    Update PrintPlayer.cs

--- PrintPlayer.cs
+++ PrintPlayer.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,39 +11,37 @@ namespace ConsoleApp38
         static void Main(string[] args)
         {
             Player player = new Player(5, 10);
-            Print print = new Print(player,'@');
+            Print print = new Print(player, '@');
             print.PrintPlayer();
         }
     }
 
     class Player
     {
-        public int _positionX { private set; get; }
-        public int _positionY { private set; get; }
+        public int PositionX { private set; get; }
+        public int PositionY { private set; get; }
 
         public Player(int positionX, int positionY)
         {
-            _positionX = positionX;
-            _positionY = positionY;
+            PositionX = positionX;
+            PositionY = positionY;
         }
     }
 
-    class Print 
+    class Print
     {
         public Player player;
-        char symbolOfPlayer;
-        public Print(Player player,char symbolOfPlayer)
+        private char _symbolOfPlayer;
+        public Print(Player player, char symbolOfPlayer)
         {
             this.player = player;
-            this.symbolOfPlayer = symbolOfPlayer;
+            this._symbolOfPlayer = symbolOfPlayer;
 
         }
         public void PrintPlayer()
         {
-            Console.SetCursorPosition(player._positionX, player._positionY);
-            Console.WriteLine(symbolOfPlayer);
+            Console.SetCursorPosition(player.PositionX, player.PositionY);
+            Console.WriteLine(_symbolOfPlayer);
         }
     }
-
-
 }
