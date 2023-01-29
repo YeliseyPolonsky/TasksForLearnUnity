cc43c76be2ae88418eefc92ecea1f9f5044157fe
YeliseyPolonsky <121894577+YeliseyPolonsky@users.noreply.github.com>
2023-01-29 18:07:45 +0300

    Update BanPlayer.cs

--- BanPlayer.cs
+++ BanPlayer.cs
@@ -15,14 +15,14 @@ namespace DataBase
     class Player
     {
         public int Number { get; private set; }
-        private string _name;
+        public string Name { get; private set; }
         private bool _isBaned;
         private int _level;
 
         public Player(int number, string name, int level)
         {
             Number = number;
-            _name = name;
+            Name = name;
             _isBaned = false;
             _level = level;
         }
@@ -40,7 +40,7 @@ namespace DataBase
 
     class Database
     {
-        private List<Player> _dataBase = new List<Player>();
+        private List<Player> _players = new List<Player>();
 
         public void StartWork()
         {
@@ -50,14 +50,16 @@ namespace DataBase
             const int OPTION_UNBAN_PLAYER = 4;
 
             bool isWorking = true;
-            
-            while(isWorking)
+
+            while (isWorking)
             {
                 Console.WriteLine("Что вы хотите сделать:\n" +
                 $"{OPTION_ADD_PLAYER} - добавить игрока;\n" +
                 $"{OPTION_DELETE_PLAYER} - удалить игрока;\n" +
                 $"{OPTION_BAN_PLAYER} - забанить игрока;\n" +
                 $"{OPTION_UNBAN_PLAYER} - разбанить игрока;\n");
+
+                ShowPlayers();
 
                 switch (GetNumber())
                 {
@@ -84,82 +86,83 @@ namespace DataBase
 
                 Console.ReadLine();
                 Console.Clear();
-            }    
+            }
         }
 
         private void AddPlayer()
         {
-            int number = GetNumberOfPlayer();
+            int number = SetNumber();
             Console.Write("Имя игрока: ");
             string name = Console.ReadLine();
             Console.WriteLine();
             Console.Write("Уровень игрока: ");
             int level = GetNumber();
-            _dataBase.Add(new Player(number, name, level));
+            Console.WriteLine("Уникальный номер данного игрока : "+number);
+            _players.Add(new Player(number, name, level));
         }
 
         private void DeletePlayer()
         {
-            int number = GetNumberOfPlayer();
-            bool isMake = false;
+            int number = GetPlayerNumber();
+            bool wasDone = false;
 
-            foreach (Player player in _dataBase)
+            foreach (Player player in _players)
             {
                 if (player.Number == number)
                 {
-                    _dataBase.Remove(player);
-                    isMake = true;
+                    _players.Remove(player);
+                    wasDone = true;
                 }
             }
 
-            if(isMake == false)
+            if (wasDone == false)
                 Console.WriteLine("Игрока с таким номером не существует!");
         }
 
         private void UnBan()
         {
-            int number = GetNumberOfPlayer();
-            bool isMake = false;
+            int number = GetPlayerNumber();
+            bool wasDone = false;
 
-            foreach (Player player in _dataBase)
+            foreach (Player player in _players)
             {
                 if (player.Number == number)
                 {
                     player.UnBanPlayer();
-                    isMake = true;
+                    wasDone = true;
                 }
             }
 
-            if (isMake == false)
+            if (wasDone == false)
                 Console.WriteLine("Игрока с таким номером не существует!");
         }
 
         private void Ban()
         {
-            int number = GetNumberOfPlayer();
-            bool isMake = false;
+            int number = GetPlayerNumber();
+            bool wasDone = false;
 
-            foreach (Player player in _dataBase)
+            foreach (Player player in _players)
             {
                 if (player.Number == number)
                 {
                     player.BanPlayer();
-                    isMake = true;
+                    wasDone = true;
                 }
             }
 
-            if (isMake == false)
+            if (wasDone == false)
                 Console.WriteLine("Игрока с таким номером не существует!");
         }
 
         private int GetNumber()
         {
-            bool isWork = true;
+            bool isWorking = true;
             int result = 0;
 
-            while (isWork)
+            while (isWorking)
                 if (int.TryParse(Console.ReadLine(), out result))
-                    isWork = false;
+                    isWorking = false;
                 else
                     Console.WriteLine("Ошибка!");
 
@@ -167,12 +170,51 @@ namespace DataBase
             return result;
         }
 
-        private int GetNumberOfPlayer()
+        private int GetPlayerNumber()
         {
             Console.Write("Введите номер игрока : ");
             int number = GetNumber();
-            
+
             return number;
+        }
+
+        private int SetNumber()
+        {
+            Random rand = new Random();
+            bool isWorking = true;
+            int countTheSameNumbers = 0;
+            int number = 0;
+
+            while (isWorking)
+            {
+                number = rand.Next(101);
+                foreach (Player player in _players)
+                {
+                    if (player.Number == number)
+                    {
+                        countTheSameNumbers++;
+                    }
+                }
+                if (countTheSameNumbers == 0)
+                    isWorking = false;
+            }
+
+            return number;
+        }
+
+        private void ShowPlayers()
+        {
+            Console.SetCursorPosition(40, 0);
+            Console.WriteLine("Игроки:");
+            int line = 0;
+
+            foreach (Player player in _players)
+            {
+                Console.SetCursorPosition(40, ++line);
+                Console.Write(player.Name);
+            }
+
+            Console.SetCursorPosition(0, 7);
         }
     }
 }
