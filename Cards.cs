46945bd21d9390a2ea579d08bdbc1834b1768d1f
YeliseyPolonsky <121894577+YeliseyPolonsky@users.noreply.github.com>
2023-01-30 16:34:27 +0300

    Update Cards.cs

--- Cards.cs
+++ Cards.cs
@@ -1,79 +1,136 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Triangles
+namespace Cards
 {
     class Program
     {
         static void Main(string[] args)
         {
-            ChangeBackgroundColor(ConsoleColor.DarkCyan);
-            ChangeForegroundColor(ConsoleColor.Yellow);
-            Console.Write("Введите:\n1 - обычный треугольник.\n2 - перевернутый треуголник.\n\nВаш выбор: ");
-            int EulerAngel = Convert.ToInt32(Console.ReadLine());
-            
+            const int OPTION_GET_CARD = 1;
+            const int OPTION_SHOW_CARDS = 2;
+            const int OPTION_EXIT = 3;
+            bool isWork = true;
+            Player player = new Player();
 
-            switch (EulerAngel)
+            Console.WriteLine("Приветствую!");
+
+            while(isWork)
             {
-                case 1:
-                    int CountLine = AskQuationOfHeight();
-                    ChangeForegroundColor(ConsoleColor.Green);
-                    int CountOfStarsAtLine = 1;
-                    for (int i = 0; i < CountLine; i++)
-                    {
+                Console.WriteLine("Выберите действие: \n" +
+                    $"{OPTION_GET_CARD} - взять карту;\n" +
+                    $"{OPTION_SHOW_CARDS} - вскрыть карты(показать)\n" +
+                    $"{OPTION_EXIT} - выйти;");
 
-                        for (int j = 0; j < CountOfStarsAtLine; j++)
-                        {
-                            Console.Write("*");
-                        }
-                        ++CountOfStarsAtLine;
-                        Console.WriteLine();
-                    }
-                    break;
-                case 2:
-                    CountLine = AskQuationOfHeight();
-                    ChangeForegroundColor(ConsoleColor.Green);
-                    CountOfStarsAtLine = CountLine;
-                    for (int i = 0; i < CountLine; i++)
-                    {
-                        for (int j = 0; j < CountOfStarsAtLine; j++)
-                        {
-                            Console.Write("*");
-                        }
-                        CountOfStarsAtLine--;
-                        Console.WriteLine();
-                    }
-                    break;
-                default:
-                    Console.Write("Вы ввели некоректное число");
-                    break;
+                switch (GetNumber())
+                {
+                    case OPTION_GET_CARD:
+                        player.GetCard();
+                        break;
+
+                    case OPTION_SHOW_CARDS:
+                        player.ShowCards();
+                        break;
+
+                    case OPTION_EXIT:
+                        isWork = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Вы ввели некоректное значение!");
+                        break;
+                }
+
+                Console.Write("Нажмите любую клавишу для продолжения.");
+                Console.ReadKey();
+                Console.Clear();
             }
-            ChangeForegroundColor(ConsoleColor.Yellow);
-            Console.WriteLine("\nГотово!");
-            Console.ReadKey(true);
         }
 
-        static void ChangeBackgroundColor(ConsoleColor backgroungColor)
+        static int GetNumber()
         {
-            Console.BackgroundColor = backgroungColor;
-            Console.Clear();
-        }
+            int result = 0;
+            bool isWorking = true;
 
-        static void ChangeForegroundColor(ConsoleColor foregroundColor)
-        {
-            Console.ForegroundColor = foregroundColor;
-        }
+            while (isWorking)
+            {
+                if (int.TryParse(Console.ReadLine(), out result))
+                    isWorking = false;
+                else
+                    Console.WriteLine("Вы ввели некоректное чтсло! Попробуйте еще раз.");
+            }
 
-        static int AskQuationOfHeight()
-        {
-            Console.Write("Какой высоты треугольник? : ");
-            int CountLine = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("\n");
-            return CountLine;
+            return result;
         }
     }
 
+    class Card
+    {
+        public string Suit { get; private set; }
+        public string Meaning { get; private set; }
+
+        public Card(string suit,string meaning)
+        {
+            Suit = suit;
+            Meaning = meaning;
+        }
+    }
+
+    class CardDeck
+    {
+        private string[] _suits = { "черви", "пики", "кресте", "бубы" };
+        private string[] _meanings = { "1", "2", "3", "4", "5", "6", "король", "дама", "валет" };
+        private List<Card> _cards = new List<Card>();
+
+        public CardDeck()
+        {
+            CreateCardDesk();
+        }
+
+        public void CreateCardDesk()
+        {
+            for (int i = 0; i < _suits.Length; i++)
+            {
+                for (int j = 0; j < _meanings.Length; j++)
+                {
+                    _cards.Add(new Card(_suits[i],_meanings[j]));
+                }
+            }
+        }
+
+        public List<Card> GetCardDesk()
+        {
+            return _cards;
+        }
+    }
+
+    class Player
+    {
+        private List<Card> _playerCards = new List<Card>();
+        private CardDeck cardDeck = new CardDeck();
+        Random rand = new Random();
+
+        public void GetCard()
+        {
+            int countCards = 36;
+
+            if(countCards>0)
+            {
+                int indexCard = rand.Next(countCards--);
+                _playerCards.Add(cardDeck.GetCardDesk()[indexCard]);
+                cardDeck.GetCardDesk().RemoveAt(indexCard);
+            }
+            else
+                Console.WriteLine("Больше карт нету!"); 
+        }
+
+        public void ShowCards()
+        {
+            foreach (Card card in _playerCards)
+            {
+                Console.Write(card.Suit+" ");
+                Console.WriteLine(card.Meaning);
+            }
+        }
+    }
 }
